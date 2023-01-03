c9bd9f2b902d46b5649740b1628687c4b4bcfed3
ma huu  du <mahuudunt@gmail.com>
2023-01-03 20:14:21 +0700

    feat(vue): add storybook motion && spinner

--- packages/components/motion/stories/motion.stories.ts
+++ packages/components/motion/stories/motion.stories.ts
@@ -1,5 +1,11 @@
 import type { StoryFn } from "@storybook/vue3";
 import { LMotion, LCollapse } from "../src";
+import { LStack, LHStack, LText } from "@luniand-ui/layout/";
+import { LButton } from "@luniand-ui/button";
+import { ref, computed } from "vue";
+import { LAnimatePresence } from "../src";
+import type { LMotionVariant } from "../src/motion-utils";
+import { TransitionVariants } from "../src/motion-utils";
 
 export default {
   title: "Components / Motion",
@@ -7,51 +13,199 @@ export default {
 };
 
 const Template: StoryFn = (args: any) => ({
-  components: { LMotion, LCollapse },
+  components: { LMotion, LCollapse, LText, LButton },
   setup() {
-    return { args };
-  },
-  template: `<h2> LMotion </h2>
-    <LMotion type="fade">
-      <div
-        bg="teal.100"
-        px="4"
-        py="4"
-        font-weight="bold"
-        rounded="md"
-        shadow="lg"
-      >
-        Fade component
-      </div>
-    </LMotion>
+    const open = ref(false);
 
-    <LMotion type="scale">
+    const toggle = () => {
+      open.value = !open.value;
+    };
+    return { args, open, toggle };
+  },
+  template: `
+    <LButton @click="toggle">With Collapse</LButton>
+    <LCollapse :is-open="open">
       <div
-        bg="teal.100"
-        px="4"
-        py="4"
-        font-weight="bold"
-        rounded="md"
-        shadow="lg"
+      :style="{
+          'background' : 'red',
+          'padding': '30px',
+          'color': 'white',
+          'fontWeight': 'bold',
+        }"
       >
-        Scale
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only five
+          centuries, but also the leap into electronic typesetting, remaining
+          essentially unchanged. It was popularised in the 1960s with the release
+          of Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including versions
+          of Lorem Ipsum.
+        </p>
       </div>
-    </LMotion>
-    <h2> collaspe </h2>
-    <LCollapse>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-  </LCollapse>
+    </LCollapse>
+    <LText>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book.
+    </LText>
   `,
 });
 
 export const Basic = Template.bind({});
+
+const ScaleTemplate: StoryFn = (args: any) => ({
+  components: { LMotion, LCollapse, LText, LButton },
+  setup() {
+    const open = ref(false);
+
+    const toggle = () => {
+      open.value = !open.value;
+    };
+    return { args, open, toggle };
+  },
+  template: `
+  <div>
+    <LButton @click="toggle" mb="10"> Toggle Scale </LButton>
+
+    <LMotion type="scale">
+      <div
+        :style="{
+          'background' : '#B2DFDB',
+          'padding': '30px',
+          'color': 'white',
+          'fontWeight': 'bold',
+        }"
+        v-if="open"
+      >
+        Scale component
+      </div>
+    </LMotion>
+  </div>
+`,
+});
+
+export const ScaleMotion = ScaleTemplate.bind({});
+
+const fadeTemplate: StoryFn = (args: any) => ({
+  components: { LMotion, LCollapse, LText, LButton },
+  setup() {
+    const open = ref(false);
+
+    const toggle = () => {
+      open.value = !open.value;
+    };
+    return { args, open, toggle };
+  },
+  template: `
+  <div>
+    <LButton @click="toggle" mb="10"> Toggle Fade </LButton>
+
+    <LMotion type="fade">
+      <div
+        :style="{
+          'background' : '#B2DFDB',
+          'padding': '30px',
+          'color': 'white',
+          'fontWeight': 'bold',
+        }"
+        v-if="open"
+      >
+        Fade component
+      </div>
+    </LMotion>
+  </div>
+`,
+});
+
+export const FadeMotion = fadeTemplate.bind({});
+
+const animateTemplate: StoryFn = (args: any) => ({
+  components: {
+    LMotion,
+    LCollapse,
+    LText,
+    LButton,
+    LHStack,
+    LStack,
+    LAnimatePresence,
+  },
+  setup() {
+    const open = ref(false);
+    const activeVariant = ref("slideLeft");
+
+    const toggle = () => {
+      open.value = !open.value;
+    };
+
+    const variants = computed(
+      () => Object.keys(TransitionVariants) as LMotionVariant[]
+    );
+
+    const handleOnBeforeEnter = (...args: any[]) => {
+      console.log("handleOnBeforeEnter", ...args);
+    };
+    const handleOnAfterEnter = (...args: any[]) => {
+      console.log("handleOnAfterEnter", ...args);
+    };
+
+    const setVariant = (variant: any) => {
+      activeVariant.value = variant;
+    };
+
+    return {
+      args,
+      open,
+      toggle,
+      activeVariant,
+      variants,
+      handleOnBeforeEnter,
+      handleOnAfterEnter,
+      setVariant,
+    };
+  },
+  template: `
+    <LStack>
+      <LButton align-self="start" @click="toggle">
+        Toggle Presence
+      </LButton>
+
+      <LHStack>
+        <!-- @ts-ignore -->
+        <LButton
+          v-for="(variant, i) in variants"
+          :key="i"
+          @click="setVariant(variant)"
+          size="xs"
+        >
+          {{ variant }}
+        </LButton>
+      </LHStack>
+
+      <LText> Active variant: {{ activeVariant }} </LText>
+    </LStack>
+    <!-- <c-portal> -->
+    <LAnimatePresence
+      :type="activeVariant"
+      @before-enter="handleOnBeforeEnter"
+      @after-enter="handleOnAfterEnter"
+    >
+      <div
+        :style="{
+          'background' : '#B2DFDB',
+          'padding': '30px',
+          'color': 'white',
+          'fontWeight': 'bold',
+        }"
+        v-if="open"
+      >
+        {{ activeVariant }} component
+      </div>
+    </LAnimatePresence>
+`,
+});
+
+export const animatePresence = animateTemplate.bind({});
--- packages/components/spinner/stories/spinner.stories.ts
+++ packages/components/spinner/stories/spinner.stories.ts
@@ -12,12 +12,42 @@ const Template: StoryFn<typeof LSpinner> = (args: any) => ({
     return { args };
   },
   template: `
-  <LSpinner size='xs' />
-  <LSpinner size='sm' />
-  <LSpinner size='md' />
-  <LSpinner size='lg' />
-  <LSpinner size='xl' />
+  <LSpinner />
   `,
 });
 
 export const Basic = Template.bind({});
+
+const SizeTemplate: StoryFn<typeof LSpinner> = (args: any) => ({
+  components: { LSpinner },
+  setup() {
+    return { args };
+  },
+  template: `
+    <LSpinner size="xs" />
+    <LSpinner size="sm" />
+    <LSpinner size="md" />
+    <LSpinner size="lg" />
+    <LSpinner size="xl" />
+  `,
+});
+
+export const Size = SizeTemplate.bind({});
+
+const SpinnerColorTemplate: StoryFn<typeof LSpinner> = (args: any) => ({
+  components: { LSpinner },
+  setup() {
+    return { args };
+  },
+  template: `
+    <LSpinner
+      thickness="4px"
+      speed="0.65s"
+      empty-color="teal"
+      color="red"
+      size="xl"
+    />
+  `,
+});
+
+export const SpinnerColor = SpinnerColorTemplate.bind({});
